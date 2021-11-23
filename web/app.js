775d4c8a3e76cda4e7961a0a0c991931744d336e
HossamGouda <hos.gouda90@gmail.com>
2021-11-24 00:54:24 +0300

    added get and post methd for api data

--- web/app.js
+++ web/app.js
@@ -12,5 +12,44 @@ async function getHolidyas() {
     const url = `https://holidays.abstractapi.com/v1/?api_key=${key}&country=${country}&year=${year}&month=${month}&day=${day}`;
     const response= await fetch(url);
     let data=await response.json();
-    console.log(data)
+    let dayEvent=data[0].name;
+    let type=data[0].type;
+    console.log(dayEvent)
+    console.log(type)
 }
+
+async function saveData() {
+    await fetch('/saveData',{
+        method:'Post',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name:data[0].name,
+            type:data[0].type
+        })
+    })   
+}
+
+
+async function getData(){
+    await fetch("/getData");
+    let final = await response.json()
+    const results = async () => {
+        const x = await final;
+    };
+    results();
+}
+
+async function udpateUx() {
+    const request = await fetch('/getData');
+    try {
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = allData.feelings;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
